3651bfb92481aafac5857f3c100b7f2f8dadb50d
Oliver Zell <zOadT@users.noreply.github.com>
2026-04-04 12:43:29 +0200

    refactor: extract register module source from typescript file

---
+++ packages/shared/src/registerSource.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from "node:fs" with { type: "macro" };
+import { transformSync } from "rolldown/utils" with { type: "macro" };
+
+// Kudos to evanw for figuring out the registry strategy in https://github.com/evanw/tla-fuzzer
+export default transformSync(
+	"./register.ts",
+	// we can't use import.meta.url here because this file is transformed by unplugin-parcel-macros
+	readFileSync(
+		process.env.TEST
+			? "./packages/shared/src/assets/register.ts"
+			: "../shared/src/assets/register.ts",
+		"utf-8",
+	),
+).code;
